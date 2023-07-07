b7e00d6f1a32776ef5583f6e4c562223210d18e0
Gregor Haslinger <kontakt@gregorsart.de>
2023-07-07 13:42:47 +0200

    feat (added show and hide functionality for the first question on the home page)

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,14 +5,20 @@ console.log("hello newbie");
 
 // VARIABLE DECLARATION
 
+// Lightmode / Darkmode
 const htmlBodyTag = document.querySelector('[data-js="body"]');
 const htmlSections = document.querySelectorAll("section");
-
 const darkmodeToggleButton = document.querySelector(
   '[data-js="darkmodeToggleButton"]'
 );
 
+// Show Answer / Hide Answer
+const answerElement = document.querySelector('[data-js="answer"]');
+const showAnswerButton = document.querySelector('[data-js="showAnswerButton"]');
+
 // EVENT LISTENER & LOGIC
+
+// Lightmode / Darkmode
 
 darkmodeToggleButton.addEventListener("click", () => {
   const htmlCollection = htmlBodyTag.children;
@@ -20,4 +26,19 @@ darkmodeToggleButton.addEventListener("click", () => {
     item.classList.toggle("lightmode");
   }
   [...htmlSections].forEach((section) => section.classList.toggle("lightmode"));
+});
+
+// Show Answer / Hide Answer
+console.log([showAnswerButton]);
+showAnswerButton.addEventListener("click", () => {
+  console.log("click!");
+  if (showAnswerButton.innerText === "Show Answer") {
+    showAnswerButton.innerText = "Hide Answer";
+    answerElement.classList.remove("hidden");
+    answerElement.classList.add("visible");
+  } else {
+    showAnswerButton.innerText = "Show Answer";
+    answerElement.classList.remove("visible");
+    answerElement.classList.add("hidden");
+  }
 });
