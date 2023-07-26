7943f610bcb2de58772e3719569dfe7868f81819
Gregor Haslinger <kontakt@gregorsart.de>
2023-07-26 10:56:27 +0200

    fix (refacotring and encapsulation for toggleAnswer functionality)

---
+++ js/toggleAnswer.js
@@ -0,0 +1,37 @@
+"use strict";
+console.log("hello newbie, toggleAnswer");
+
+// Show Answer / Hide Answer Animation
+// Works for all the Cards that get added
+
+function toggleAnswer(card) {
+  const showAnswerButton = card.querySelector('[data-js="showAnswerButton"]');
+  showAnswerButton.addEventListener("click", () => {
+    const answerElement = card.querySelector('[data-js="answer"]');
+
+    if (showAnswerButton.innerText === "Show Answer") {
+      // animatioin
+      answerElement.classList.remove("hidden");
+      answerElement.classList.add("visible");
+      // change button text
+      showAnswerButton.innerText = "Hide Answer";
+      // remove hidden attribute
+      answerElement.removeAttribute("hidden");
+    } else {
+      // animation
+      answerElement.classList.remove("visible");
+      answerElement.classList.add("hidden");
+      // wait until animation has ended, and then set hidden and change the button text
+      setTimeout(() => {
+        answerElement.setAttribute("hidden", true);
+        // change button text
+        showAnswerButton.innerText = "Show Answer";
+      }, 500);
+    }
+  });
+}
+
+const htmlCards = document.querySelectorAll('[data-js="card"]');
+[...htmlCards].forEach((card) => {
+  toggleAnswer(card);
+});
