10ce8f335352e31ce5a99d9e0a064bd264d27111
Gregor Haslinger <kontakt@gregorsart.de>
2023-08-01 11:43:47 +0200

    add (prepopulate quiz app with two cards)

--- js/index.js
+++ js/index.js
@@ -1,7 +1,28 @@
 "use strict";
 console.log("hello newbie, index");
 
+// localStorage.clear();
+
+// Prepopulate entries
+const prepopulatedCards = [
+  {
+    id: crypto.randomUUID(),
+    question: "Welcher Studio Ghibli Film entstand 1997?",
+    answer: "Prinzessin Mononoke",
+    bookmarked: false,
+    tags: "film",
+  },
+  {
+    id: crypto.randomUUID(),
+    question: "Wie viele Oskars hat Studio Ghibli bis jetzt gewonnen?",
+    answer: "Einen",
+    bookmarked: false,
+    tags: "film",
+  },
+];
+
 // get from local storage or empty array
-const cards = JSON.parse(localStorage.getItem("cards")) ?? [];
+const cards = JSON.parse(localStorage.getItem("cards")) ?? prepopulatedCards;
+localStorage.setItem("cards", JSON.stringify(cards));
 // call function from renderCards.js
 renderCardsConditionally(cards);
